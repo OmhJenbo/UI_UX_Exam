b8f8ca86156ff675d1ec0c198ad2378b2cf67894
Marie Kjærside <m.kjaerside@outlook.com>
2024-12-05 14:07:20 +0100

    logout added to most pages, and also footer

--- js/logout.js
+++ js/logout.js
@@ -15,10 +15,12 @@ if (userId() == 0) {
     document.querySelector("#login").classList.remove("hidden");
     document.querySelector("#signup").classList.remove("hidden");
     document.querySelector("#logout").classList.add("hidden");
+    document.querySelector("#settings").classList.add("hidden");
 } else {
     document.querySelector("#login").classList.add("hidden");
     document.querySelector("#signup").classList.add("hidden");
     document.querySelector("#logout").classList.remove("hidden");
+    document.querySelector("#settings").classList.remove("hidden");
 }
 // When the logout button is clicked it goes to the function logout
 document.getElementById("logout").addEventListener("click", () => logout());
